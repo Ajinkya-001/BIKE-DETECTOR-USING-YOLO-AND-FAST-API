5510d673ff9c14d8114410be211661c3680a56b5
Ajinkya-001 <ajinkyapatilckl@gmail.com>
2025-07-14 22:49:56 +0530

    Update detector.py

--- app/detector.py
+++ app/detector.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 from PIL import Image
 import numpy as np
 
-model = YOLO("app/yolov8s.pt") 
+model = YOLO("bike-detector.pt") 
 def predict(image: Image.Image):
     results = model.predict(image)
     return results[0]
